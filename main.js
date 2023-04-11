ad5d4febd8fdbd577cce0397cdb1f053ffc5a6f0
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-04-11 19:21:01 -0300

    Add light, floor and character (the ball)

--- main.js
+++ main.js
@@ -14,11 +14,20 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 )
+camera.position.z = 12
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
+
+// Light
+//***********************************************************************
+const light = new THREE.AmbientLight(0x404040) // soft white light
+scene.add(light)
+// White directional light at half intensity shining from the top.
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+scene.add(directionalLight)
 
 // Cube
 //***********************************************************************
@@ -27,7 +36,25 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-camera.position.z = 5
+// Ball character
+//***********************************************************************
+const ballGeometry = new THREE.SphereGeometry()
+const ballMaterial = new THREE.MeshBasicMaterial({ color: 0xfffff0 })
+const sphere = new THREE.Mesh(ballGeometry, ballMaterial)
+sphere.position.set(4, 0, 1)
+scene.add(sphere)
+
+// Floor
+//***********************************************************************
+const floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10)
+const floorMaterial = new THREE.MeshBasicMaterial({
+  color: 0x228b22,
+  side: THREE.DoubleSide,
+})
+const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial)
+floorMesh.rotation.x = Math.PI / 2
+floorMesh.position.y = -5
+scene.add(floorMesh)
 
 // Rendering the scene
 //***********************************************************************
