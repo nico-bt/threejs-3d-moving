00a016253fd542b8e372d98aa51a9fe9e8fa62f0
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-04-13 02:32:38 -0300

    Add textures

--- main.js
+++ main.js
@@ -22,17 +22,27 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
 
+// Background Texture
+//***********************************************************************
+const spaceTexture = new THREE.TextureLoader().load("space.jpg")
+scene.background = spaceTexture
+
 // Light
 //***********************************************************************
 const light = new THREE.AmbientLight(0x404040) // soft white light
 scene.add(light)
 
-// White directional light.
+// // White directional light.
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
 directionalLight.position.x = -10
 directionalLight.position.y = 50
 directionalLight.position.z = 2
 scene.add(directionalLight)
+
+// PointLight inside Torus geometry
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(-20, 20, -20)
+scene.add(pointLight)
 
 // Cubes
 //***********************************************************************
@@ -40,6 +50,7 @@ const geometry = new THREE.BoxGeometry(2, 2, 2)
 const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 })
 const cube = new THREE.Mesh(geometry, material)
 cube.position.x = -5
+cube.position.y = 1
 scene.add(cube)
 
 const cube2 = new THREE.Mesh(geometry, material)
@@ -61,10 +72,7 @@ scene.add(rectangle)
 // Torus
 //***********************************************************************
 const torusGeometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const torusMaterial = new THREE.MeshBasicMaterial({
-  color: 0xff6347,
-  wireframe: true,
-})
+const torusMaterial = new THREE.MeshStandardMaterial({ color: 0xff6347 })
 const torus = new THREE.Mesh(torusGeometry, torusMaterial)
 torus.position.set(-20, 20, -20)
 scene.add(torus)
@@ -81,14 +89,29 @@ scene.add(sphere)
 // Floor
 //***********************************************************************
 const floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10)
-const floorMaterial = new THREE.MeshBasicMaterial({
-  color: 0x228b22,
+
+const moonTexture = new THREE.TextureLoader().load("moon.jpg")
+const normalTexture = new THREE.TextureLoader().load("normal.jpg")
+
+const floorMaterial = new THREE.MeshStandardMaterial({
+  // color: 0x003b2b,
+  map: moonTexture,
+  normalMap: normalTexture,
   side: THREE.DoubleSide,
 })
 const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial)
 floorMesh.rotation.x = Math.PI / 2
-floorMesh.position.y = -5
+floorMesh.position.y = -1
 scene.add(floorMesh)
+
+// Moon
+//***********************************************************************
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({ map: moonTexture })
+)
+moon.position.set(20, 14, -25)
+scene.add(moon)
 
 // Moving the character
 // Left: 37, Up: 38, Right: 39, Down: 40, Space: 32
